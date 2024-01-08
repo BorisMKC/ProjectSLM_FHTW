93721c06889f9f26ac93cf69370f2f81eb2b4ca4
BIC-091 <ic22b091@technikum-wien.at>
2024-01-08 20:39:51 +0100

    added necessary classes

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Player {
+    public char marker;
+
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    public char getMarker() {
+        return this.marker;
+    }
+}
